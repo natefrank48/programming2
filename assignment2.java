bb2a92684de1b97bd8a6f2e47317aa56e4f7be74
abes <abes@pyrite-n1.cs.iastate.edu>
2022-11-29 13:48:24 -0600

    complete R instruction logic

--- assignment2.java
+++ assignment2.java
@@ -12,34 +12,34 @@ public class assignment2{
         try (
             InputStream inputStream = new FileInputStream(args[0]);
         ) {
-            HashMap<String, Instruction> hm = new HashMap<String, Instruction>();
-            hm.put("10001011000", new Instruction("ADD", 'R'));
-            hm.put("1001000100", new Instruction("ADDI", 'I'));
-            hm.put("10001010000", new Instruction("AND", 'R'));
-            hm.put("1001001000", new Instruction("ANDI", 'I'));
-            hm.put("000101", new Instruction("B", 'B'));
-            hm.put("01010100", new Instruction("B.cond", 'C'));
-            hm.put("100101", new Instruction("BL", 'B'));
-            hm.put("11010110000", new Instruction("BR", 'R'));
-            hm.put("10110100", new Instruction("CBNZ", 'C'));
-            hm.put("10110101", new Instruction("CBZ", 'C'));
-            hm.put("11001010000", new Instruction("EOR", 'R'));
-            hm.put("1101001000", new Instruction("EORI", 'I'));
-            hm.put("11111000010", new Instruction("LDUR", 'D'));
-            hm.put("11010011011", new Instruction("LSL", 'R'));
-            hm.put("11010011010", new Instruction("LSR", 'R'));
-            hm.put("10101010000", new Instruction("ORR", 'R'));
-            hm.put("1011001000", new Instruction("ORRI", 'I'));
-            hm.put("11111000000", new Instruction("STUR", 'D'));
-            hm.put("11001011000", new Instruction("SUB", 'R'));
-            hm.put("1101000100", new Instruction("SUBI", 'I'));
-            hm.put("1111000100", new Instruction("SUBIS", 'I'));
-            hm.put("11101011000", new Instruction("SUBS", 'R'));
-            hm.put("10011011000", new Instruction("MUL", 'R'));
-            hm.put("11111111101", new Instruction("PRNT", 'R'));
-            hm.put("11111111100", new Instruction("PRNL", 'R'));
-            hm.put("11111111110", new Instruction("DUMP", 'R'));
-            hm.put("11111111111", new Instruction("HALT", 'R'));
+            HashMap<String, Instruction> opcodeToInstruction = new HashMap<String, Instruction>();
+            opcodeToInstruction.put("10001011000", new Instruction("ADD", 'R'));
+            opcodeToInstruction.put("1001000100", new Instruction("ADDI", 'I'));
+            opcodeToInstruction.put("10001010000", new Instruction("AND", 'R'));
+            opcodeToInstruction.put("1001001000", new Instruction("ANDI", 'I'));
+            opcodeToInstruction.put("000101", new Instruction("B", 'B'));
+            opcodeToInstruction.put("01010100", new Instruction("B.cond", 'C'));
+            opcodeToInstruction.put("100101", new Instruction("BL", 'B'));
+            opcodeToInstruction.put("11010110000", new Instruction("BR", 'R'));
+            opcodeToInstruction.put("10110100", new Instruction("CBNZ", 'C'));
+            opcodeToInstruction.put("10110101", new Instruction("CBZ", 'C'));
+            opcodeToInstruction.put("11001010000", new Instruction("EOR", 'R'));
+            opcodeToInstruction.put("1101001000", new Instruction("EORI", 'I'));
+            opcodeToInstruction.put("11111000010", new Instruction("LDUR", 'D'));
+            opcodeToInstruction.put("11010011011", new Instruction("LSL", 'R'));
+            opcodeToInstruction.put("11010011010", new Instruction("LSR", 'R'));
+            opcodeToInstruction.put("10101010000", new Instruction("ORR", 'R'));
+            opcodeToInstruction.put("1011001000", new Instruction("ORRI", 'I'));
+            opcodeToInstruction.put("11111000000", new Instruction("STUR", 'D'));
+            opcodeToInstruction.put("11001011000", new Instruction("SUB", 'R'));
+            opcodeToInstruction.put("1101000100", new Instruction("SUBI", 'I'));
+            opcodeToInstruction.put("1111000100", new Instruction("SUBIS", 'I'));
+            opcodeToInstruction.put("11101011000", new Instruction("SUBS", 'R'));
+            opcodeToInstruction.put("10011011000", new Instruction("MUL", 'R'));
+            opcodeToInstruction.put("11111111101", new Instruction("PRNT", 'R'));
+            opcodeToInstruction.put("11111111100", new Instruction("PRNL", 'R'));
+            opcodeToInstruction.put("11111111110", new Instruction("DUMP", 'R'));
+            opcodeToInstruction.put("11111111111", new Instruction("HALT", 'R'));
 
 
             HashMap<Integer, String> labels = new HashMap<Integer, String>();
@@ -54,17 +54,26 @@ public class assignment2{
                     int in = inputStream.read();
                     s += String.format("%8s", Integer.toBinaryString(in)).replaceAll(" ", "0");
                 }
-                if(hm.containsKey(s.substring(0,6))){
-                    System.out.println(hm.get(s.substring(0,6)).name);
+                if(opcodeToInstruction.containsKey(s.substring(0,6))){
+                    System.out.println(opcodeToInstruction.get(s.substring(0,6)).name);
                 }
-                else if(hm.containsKey(s.substring(0,8))){
-                    System.out.println(hm.get(s.substring(0,8)).name);
+                else if(opcodeToInstruction.containsKey(s.substring(0,8))){
+                    System.out.println(opcodeToInstruction.get(s.substring(0,8)).name);
                 }
-                else if(hm.containsKey(s.substring(0,10))){
-                    System.out.println(hm.get(s.substring(0,10)).name);
+                else if(opcodeToInstruction.containsKey(s.substring(0,10))){
+                    System.out.println(opcodeToInstruction.get(s.substring(0,10)).name);
                 }
-                else if(hm.containsKey(s.substring(0,11))){
-                    System.out.println(hm.get(s.substring(0,11)).name);
+                else if(opcodeToInstruction.containsKey(s.substring(0,11))){
+                    String instructionType = opcodeToInstruction.get(s.substring(0,11)).type
+                    
+                    String instruction;
+                    if (instructionType == 'D') {
+                        instruction = getDTypeInstruction(s);
+                    } else {
+                        instruction = getRTypeInstruction(s);
+                    }
+
+                    System.out.println(instruction);
                 }
             }  
         } catch (IOException ex) {
@@ -74,9 +83,31 @@ public class assignment2{
         
     }
 
-    private static String getRTypeInstruction(String instructionName, String binaryInstruction){
-        //todo
-        return "";
+    private static String getRTypeInstruction(String binaryInstruction){
+        String instructionName = opcodeToInstruction.get(s.substring(0,11)).name;
+        int rm = Integer.parseInt(s.substring(11, 16), 2);
+        int shamt = Integer.parseInt(s.substring(16, 22), 2);
+        int rn = Integer.parseInt(s.substring(22, 27), 2);
+        int rd = Integer.parseInt(s.substring(27), 2);
+        
+        String instruction;
+        if (instructionName == "LSL" || instructionName == "LSR") {
+            instruction = instructionName + " " + numberToRegister(rd) + ", " + numberToRegister(rn) + ", #" + shamt;
+        } else if (instructionName == "BR") {
+            instruction = instructionName + " " + numberToRegister(rn);
+        } else if (instructionName == "PRNT") {
+            instruction = "PRNT";
+        } else if (instructionName == "PRNL") {
+            instruction = "PRNL";
+        } else if (instructionName == "DUMP") {
+            instruction = "DUMP";
+        } else if (instructionName == "HALT") {
+            instruction = "HALT";
+        } else {
+            instruction = instructionName + " " + numberToRegister(rd) + ", " + numberToRegister(rn) + ", " + numberToRegister(rm);
+        }
+        
+        return instruction;
     }
 
     private static String getITypeInstruction(String instructionName, String binaryInstruction){
@@ -114,6 +145,24 @@ public class assignment2{
     private static String getRegister(String fiveBitOpcode){
         //todo
         return "";
+    }
+
+    private static String numberToRegister(int registerNumber) {
+        String registerName;
+        switch (registerNumber) {
+            case 28: registerName = "SP";
+                     break;
+            case 29: registerName = "FP";
+                     break;
+            case 30: registerName = "LR";
+                     break;
+            case 31: registerName = "XZR";
+                     break;
+            default: registerName = "X" + registerNumber;
+                     break;
+        }
+
+        return registerName;
     }
 }
 
